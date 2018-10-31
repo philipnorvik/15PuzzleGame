e844f420936634804436cebdbe935d19c4531481
PhilipN <01.philip@gmail.com>
2018-10-31 22:10:45 +0100

    commit inför merge

--- src/sample/Game.java
+++ src/sample/Game.java
@@ -1,4 +1,40 @@
 package sample;
 
-public class Game {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Game extends JFrame {
+
+    private final JButton newGame = new JButton("Nytt Spel");
+    private final JButton exit = new JButton("Avsluta");
+
+    private Board boardGame = new Board(4, 4);
+    private final Layout layout = new Layout(newGame, exit);
+
+    public void GameGUI() {
+        setLayout(new BorderLayout());
+        setTitle("15-Spel");
+        setVisible(true);
+        setSize(500, 500);
+
+        setLocationRelativeTo(null);
+        add(boardGame, BorderLayout.CENTER);
+        add(layout, BorderLayout.WEST);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        newGame.addActionListener((ActionEvent ae) -> {
+            remove(boardGame);
+            boardGame = new Board(layout.getRows(), layout.getCollums());
+            setSize(layout.getCollums() * 85 + 150, layout.getRows() * 85);
+            setLocationRelativeTo(null);
+            add(boardGame);
+            revalidate();
+            repaint();
+        });
+
+        exit.addActionListener((ActionEvent ae) -> {
+            System.exit(0);
+        });
+    }
 }
--- src/sample/Layout.java
+++ src/sample/Layout.java
@@ -1,4 +1,40 @@
 package sample;
 
-public class Layout {
+import javax.swing.*;
+import java.awt.*;
+
+public class Layout extends JPanel {
+
+    int rows = 4;
+    int collums = 4;
+
+    JPanel topPanel = new JPanel();
+    JPanel botPanel = new JPanel();
+
+
+
+
+    public Layout(JButton newGame, JButton exit) {
+
+        topPanel.setLayout(new BorderLayout());
+        botPanel.setLayout(new GridLayout(3, 1));
+        botPanel.add(newGame);
+        botPanel.add(exit);
+
+        setLayout(new GridLayout(3, 1));
+
+        add(topPanel);
+
+        add(botPanel);
+        setPreferredSize(new Dimension(150, 0));
+
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCollums() {
+        return collums;
+    }
 }
